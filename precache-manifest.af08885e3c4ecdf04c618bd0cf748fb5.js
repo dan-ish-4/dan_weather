2c274590108935b6e0b4900493ad352d58fe42ec
dan-ish-4 <khandanishwwe@gmail.com>
2022-07-21 18:17:16 +0530

    Updates

---
+++ precache-manifest.af08885e3c4ecdf04c618bd0cf748fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d6c98b21b05cd6734b9",
+    "url": "/dan_weather/static/css/main.56dc8531.chunk.css"
+  },
+  {
+    "revision": "3d6c98b21b05cd6734b9",
+    "url": "/dan_weather/static/js/main.3d6c98b2.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/dan_weather/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "f5f9605b56e0b5559460",
+    "url": "/dan_weather/static/js/2.f5f9605b.chunk.js"
+  },
+  {
+    "revision": "8b7b65b045670fdc8a61",
+    "url": "/dan_weather/static/js/runtime~main.8b7b65b0.js"
+  },
+  {
+    "revision": "00ba2f8f1cdeb6ae26d4f065d44d00e0",
+    "url": "/dan_weather/index.html"
+  }
+];
